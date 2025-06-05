97fc173c014133cf183a67621d1a9177274ff522
asenetcky <me@asenetcky.dev>
2025-06-04 21:57:39 -0400

    Squashed commit of the following:
    commit e18736e4487bab6e9dc457ecc6c738e043469583
    Author: asenetcky <me@asenetcky.dev>
    Date:   Wed Jun 4 21:57:14 2025 -0400
    
        feat: update housing
    
    commit 8ec691a8a44843bea0d1d7451358e84f9b36fbc4
    Author: asenetcky <me@asenetcky.dev>
    Date:   Wed Jun 4 21:57:01 2025 -0400
    
        feat: update workspace settings
    
    commit 24303c36a9fedd788a2fea389c40740dc071fcfc
    Author: asenetcky <me@asenetcky.dev>
    Date:   Wed Jun 4 21:56:42 2025 -0400
    
        feat: add numpy
    
    commit b86ce38cce978102ef4b8b26fb8dec65f2d094d9
    Author: asenetcky <me@asenetcky.dev>
    Date:   Mon Jun 2 21:13:13 2025 -0400
    
        feat: working through example
    
    commit ec99796f2ae4ab51a6a52de41135a86b3841e204
    Author: asenetcky <me@asenetcky.dev>
    Date:   Mon Jun 2 07:43:05 2025 -0400
    
        feat: random plot
    
    commit 1e0943dbde1f805e3fe48deb29749a26ff9c1916
    Author: asenetcky <me@asenetcky.dev>
    Date:   Mon Jun 2 07:03:16 2025 -0400
    
        chore: maybe polars
    
    commit 9a60538334c1cd66409c94d342b7dbf5098e9f61
    Author: asenetcky <me@asenetcky.dev>
    Date:   Mon Jun 2 06:59:08 2025 -0400
    
        feat: add wip code
    
    commit 68f3b3d8c89129b02dc54fceb7f4a20f6b279957
    Author: asenetcky <me@asenetcky.dev>
    Date:   Mon Jun 2 06:58:13 2025 -0400
    
        feat: add altair
    
    commit 16d2b45f0d15f99ab8c1654faf94c1e407c75859
    Author: asenetcky <me@asenetcky.dev>
    Date:   Sun Jun 1 19:59:37 2025 +0000
    
        feat: mv to ipynb
    
    commit e68645aa25d0edfb734ade46e7abf3c721f1d03c
    Author: asenetcky <me@asenetcky.dev>
    Date:   Sun Jun 1 19:41:56 2025 +0000
    
        chore: working through ane xample
    
    commit 428dac560f4f1dd8636a12e1f0d10d55d6d89e88
    Author: asenetcky <me@asenetcky.dev>
    Date:   Sun Jun 1 19:17:10 2025 +0000
    
        feat: initialize housing ml project
    
    commit 6b490b32332e0ad3c9f09ae8251a14b844d1c43b
    Author: asenetcky <me@asenetcky.dev>
    Date:   Sun Jun 1 19:16:02 2025 +0000
    
        feat: init uv project

---
+++ housing.py
@@ -0,0 +1,26 @@
+import tarfile
+import urllib.request
+from pathlib import Path
+
+import polars as pl
+
+
+def load_housing_data():
+    tarball_path = Path("datasets/housing.tgz")
+    if not tarball_path.is_file():
+        Path("datasets").mkdir(parents=True, exist_ok=True)
+        url = "https://github.com/ageron/data/raw/main/housing.tgz"
+        urllib.request.urlretrieve(url, tarball_path)
+        with tarfile.open(tarball_path) as housing_tarball:
+            housing_tarball.extractall(path="datasets")
+    return pl.read_csv(Path("datasets/housing/housing.csv"))
+
+
+housing = load_housing_data()
+
+housing.head()
+
+housing.glimpse()
+housing.describe()
+
+housing.group_by(pl.col("ocean_proximity")).len("n").sort("n", descending=True)
